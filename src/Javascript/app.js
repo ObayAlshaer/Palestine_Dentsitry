581a084a0522ca3421da18a4bea47e7fce4923df
Mohamed-Obay Alshaer <obay.alshaer@gmail.com>
2023-06-12 02:00:05 -0400

    All files for website.

---
+++ src/Javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    //HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+               
+            }
+        }
+    })
+});
+
